40c11c9e2212c9ffdf2dd94d350cba300e8fc2fe
WuNL <wunailiang@gmail.com>
2018-08-08 16:37:43 +0800

    update request

--- webRtcTools/httpSender.py
+++ webRtcTools/httpSender.py
@@ -2,5 +2,7 @@
 
 import requests
 
-r = requests.post("http://127.0.0.1:8000")
+r = requests.get("http://127.0.0.1:8080/InitEncoder/",
+                 data="""{"encoder_name": "1970-01-01", "codec": "h264", "v_height": 640, "v_width": 480, "v_gop": 300,
+                       "packetMode": 0}""")
 print(r.text)
